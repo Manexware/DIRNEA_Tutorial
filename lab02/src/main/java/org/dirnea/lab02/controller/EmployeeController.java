27c96f62210dfa7d1475177ff81f068488ae646e
mangrovex <manuel.vega@manexware.com>
2020-06-16 11:20:33 -0500

    hateoas inicial

--- lab02/src/main/java/org/dirnea/lab02/controller/EmployeeController.java
+++ lab02/src/main/java/org/dirnea/lab02/controller/EmployeeController.java
@@ -3,7 +3,9 @@ package org.dirnea.lab02.controller;
 import org.dirnea.lab02.model.Employee;
 import org.dirnea.lab02.repository.EmployeeRepository;
 import org.dirnea.lab02.util.EmployeeNotFoundException;
+import org.springframework.hateoas.Link;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
 
 import java.util.List;
 
@@ -31,7 +33,13 @@ public class EmployeeController {
     // http://localohots:8080/employees/4
     @GetMapping("/employees/{id}")
     Employee one(@PathVariable Long id) {
-        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+        Link link = new Link("http://localhost:8088/employees/" + employee.getId());
+        employee.add(link);
+        return employee;
+        //{ "id": 1, "name":"Manuel Vega", "role":"Instructor"}
+        // return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
     }
 
     // employee ---> objeto, tiene unos atributos : id, name y role
--- lab02/src/main/java/org/dirnea/lab02/model/Employee.java
+++ lab02/src/main/java/org/dirnea/lab02/model/Employee.java
@@ -1,7 +1,10 @@
 package org.dirnea.lab02.model;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,13 +12,14 @@ import javax.persistence.Id;
 
 @Data
 @Entity
-public class Employee {
+public class Employee extends RepresentationModel<Employee> {
 
     private @Id @GeneratedValue Long id;
     private String name;
     private String role;
 
-    public Employee(String name, String role) {
+    @JsonCreator //{"nombre": "-----", "cargo": "---"}
+    public Employee(@JsonProperty("nombre") String name, @JsonProperty("cargo") String role) {
         this.name = name;
         this.role = role;
     }
